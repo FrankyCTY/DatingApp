33e7921b1c9dc0fadaaa833d28b008add8a32a18
TakYuChan <frankychantakyu@yahoo.com.hk>
2020-11-28 19:07:33 +1300

    End of section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+namespace API.Entities
+{
+  public class AppUser
+  {
+    public int Id { get; set; } // Recognized by Entity Framework as Primary Key
+    public string UserName { get; set; } // Carmel Case because if ASP.net Identity naming convention 
+  }
+}
